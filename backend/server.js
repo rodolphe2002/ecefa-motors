48a9ecb983555b15cb1b3d99ea067f41ae71e8ac
Kouadio Bah Rodolphe <rodolphekouadio225@icloud.com>
2025-07-15 14:38:28 +0000

    ajout et modification des liens pour heberger

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,26 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+  'https://ecefa-motors-7q08eus2b-kouadio-bah-rodolphes-projects.vercel.app', // Ton vrai domaine Vercel
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Autoriser les outils locaux ou Vercel
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Non autorisé par la politique CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+// Pour gérer les pré-requêtes OPTIONS
+app.options('*', cors());
+
 app.use(express.json());
 
 const { MONGODB_URI, ADMIN_USERNAME, ADMIN_PASSWORD } = process.env;
